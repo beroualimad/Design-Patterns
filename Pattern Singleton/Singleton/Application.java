c3a8c02dfc60b9588eefba6e0592feb151e32739
beroualimad <beroualimad5@gmail.com>
2023-08-08 21:20:48 +0100

    Pattern Singleton

---
+++ Pattern Singleton/Singleton/Application.java
@@ -0,0 +1,10 @@
+package Singleton;
+
+public class Application {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			ThreadedTask t=new ThreadedTask("T"+i);
+			t.start();
+		}
+	}
+}
